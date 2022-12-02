d7e667046650966578467aeb84c82abd5fcc9131
Showkat Easin <68397684+ShowkatEasin@users.noreply.github.com>
2022-12-02 19:47:21 +0600

    Running

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -42,11 +42,7 @@
                 <div class="col-md-8">
                     <div class="user-menu">
                         <ul>
-                            <li><a href="#"><i class="fa fa-user"></i> My Account</a></li>
-                            <li><a href="#"><i class="fa fa-heart"></i> Wishlist</a></li>
-                            <li><a href="cart.html"><i class="fa fa-user"></i> My Cart</a></li>
-                            <li><a href="checkout.html"><i class="fa fa-user"></i> Checkout</a></li>
-                            <li><a href="#"><i class="fa fa-user"></i> Login</a></li>
+                            <li><a href="{{ route('register') }}"><h4><i class="fa fa-user"></i>Sign Up</h4> </a></li>
                         </ul>
                     </div>
                 </div>
